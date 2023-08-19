ddffa4c13b19b53b426e5fce8100af00f064ddfa
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2023-08-19 08:04:15 -0500

    chore: writing assertions in test

--- src/test/java/com/kbi/experienceapi/service/EmployeeServiceTest_Using_MockWebServer.java
+++ src/test/java/com/kbi/experienceapi/service/EmployeeServiceTest_Using_MockWebServer.java
@@ -1,5 +1,7 @@
 package com.kbi.experienceapi.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kbi.experienceapi.model.employeesworld.Employee;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -23,6 +25,7 @@ import java.util.function.Consumer;
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest_Using_MockWebServer {
     public MockWebServer mockWebServer;
+    private final ObjectMapper objectMapper = new ObjectMapper();
     /**
      * Mock Web server allows our code to use real HTTP calls to a **local endpoint**. We get the benefit of testing
      * the intended HTTP interactions and dont have to face any challenge of mocking a complex Fluent Client, like
@@ -43,24 +46,48 @@ public class EmployeeServiceTest_Using_MockWebServer {
 
     @BeforeEach
     void initialize() {
-        String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort());
+        String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort()); //port= 61664
         webClient = WebClient.builder().baseUrl(baseUrl).build();
     }
 
     @Test
-    void getEmployeesShouldReturn200() {
-        mockWebServer.enqueue(new MockResponse().setHeader("content-type", "application/json"));
-        EmployeeService employeeService = new EmployeeService(webClient);
+    void getEmployeesShouldReturn200() throws JsonProcessingException {
+        //mocked response
+        mockWebServer
+                .enqueue(
+                        new MockResponse()
+                                .setHeader("content-type", "application/json") //optional, until this service specifically specifies response header is produced by it. Check for it's controller, if it has produces : application/json
+                                .setHeader("key", "value") //if more than one header
+                                .setBody(objectMapper.writeValueAsString(employeeArrayResponseMock())));
 
-        Mono<Employee[]> employeesArrayInMono = employeeService.getEmployees();
-        StepVerifier.create(employeesArrayInMono).assertNext(new Consumer<Employee[]>() {
+        //actual response
+        EmployeeService employeeService = new EmployeeService(webClient);
+        Mono<Employee[]> actualResponseOfEmployeesArrayInMono = employeeService.getEmployees();
+
+        //assertion
+        StepVerifier.create(actualResponseOfEmployeesArrayInMono)
+                .assertNext(new Consumer<Employee[]>() {
                     @Override
                     public void accept(Employee[] t) {
                         List<Employee> employeees = Arrays.stream(t).toList();
-                        Assertions.assertEquals("Samuel", employeees.get(0).getName());
+                        Assertions.assertEquals("name1", employeees.get(0).getName());
                     }
-                });
+                })
+                .expectComplete()
+                .verify();
     }
 
+    private Employee[] employeeArrayResponseMock() {
+        //Employee[] = [  {}   ,  {},    {},     {},     {} ]
+        //names          emp0    emp1   emp2    emp3    emp4
+        Employee[] emp = new Employee[5];
+        emp[0] = new Employee("id1", "name1", 23, "software-engineer-1", 77000);
+        emp[1] = new Employee("id2", "name2", 45, "software-engineer-2", 97000);
+        emp[2] = new Employee("id3", "name3", 35, "software-engineer-3", 87000);
+        emp[3] = new Employee("id4", "name4", 19, "intern", 57000);
+        emp[4] = new Employee("id5", "name5", 27, "software-engineer-2", 67000);
+        return emp;
+
+    }
 
 }
